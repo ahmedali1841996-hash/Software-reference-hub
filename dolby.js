36925353481ec3edf86be9129a946fae1de67209
ahmedali1841996-hash <ahmedali1841996@gmail.com>
2025-11-19 14:48:58 +0200

    Update dolby.js

--- dolby.js
+++ dolby.js
@@ -1,1 +1,22 @@
-﻿
+const sidebar = document.getElementById("sidebar");
+const closeBtn = document.getElementById("close-btn");
+const menuBtn = document.getElementById("menu-btn");
+
+menuBtn.addEventListener("click", () => {
+    sidebar.style.left = "0";
+    menuBtn.classList.remove("show-hamburger");
+});
+
+closeBtn.addEventListener("click", () => {
+    sidebar.style.left = "-250px";
+    menuBtn.classList.add("show-hamburger");
+});
+
+
+const submenuItems = document.querySelectorAll(".has-submenu");
+
+submenuItems.forEach(item => {
+    item.addEventListener("click", () => {
+        item.classList.toggle("open"); // يفتح أو يقفل الفرعية
+    });
+});﻿
